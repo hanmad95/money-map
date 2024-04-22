7cab87fa9dd977fe122979ffa05b791e718e89ea
Madlinski <hannesmadl95@gmail.com>
2024-04-22 21:59:45 +0200

    Update mysql_conector.py
    Adding Docstrings

--- money_map/connect/mysql_conector.py
+++ money_map/connect/mysql_conector.py
@@ -14,9 +14,34 @@ class  MySQLConnector:
     # region [public]
     # Public variables and public methods are accessible outside the object + (staticmethods).
     def create_db_url(self,user:str,pw:str,host:str,db:str) -> str:
+        '''Generate MYSQL Database Connection URL
+
+        Parameters
+        ----------
+        user : str
+            Username
+        pw : str
+            Password
+        host : str
+            Hostname
+        db : str
+            Database Name
+
+        Returns
+        -------
+        str
+           Connection URL
+        '''
         return f"mysql+pymysql://{user}:{pw}@{host}/{db}"
 
     def create_sql_engine(self) -> Any:
+        '''Create SQLAlchemy Engine
+
+        Returns
+        -------
+        Any
+            SQLAlchemy Engine
+        '''
         load_dotenv(find_dotenv())
         return create_engine(self.create_db_url(user=os.getenv("USER"),
                                                 pw=os.getenv("PASSWORD"),
@@ -24,6 +49,8 @@ class  MySQLConnector:
                                                 db=os.getenv("DATABASE")))
 
     def create_tables(self) -> None:
+        ''' Creates all tables in defined database, if not existing.
+        '''
         Base.metadata.create_all(bind=self.create_sql_engine())
         return
 
