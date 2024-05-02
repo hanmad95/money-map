0c4f590510d834df63528abf855ee8094d139596
Madlinski <hannesmadl95@gmail.com>
2024-05-02 08:56:20 +0200

    Update categories_ingestion.py
    insert connector

--- money_map/ingestion/categories_ingestion.py
+++ money_map/ingestion/categories_ingestion.py
@@ -1,22 +1,19 @@
 # stdlib
-import os
 from pathlib import Path
-from typing import List,Any,Union
+from typing import List,Union
 
 from attrs import define
 import pandas as pd
 import json
-from dotenv import load_dotenv,find_dotenv
 
-from sqlalchemy import create_engine, select, inspect
+from sqlalchemy import select, inspect
 from sqlalchemy.orm import Session
 
 # custom class
-from money_map.models.orm_models import Transaction_Categories_Table, Base
-from money_map.models.categories import TRANSACTION_CATEGORIES
+from money_map.models.orm_models import Transaction_Categories_Table
 from money_map.ingestion.ingestor import Ingestor
+from money_map.connect.mysql_conector import MySQLConnector
 
-# TODO:  Add connector and replace not required functions
 # =============================================================================
 # Transaction Categories
 # =============================================================================
@@ -27,25 +24,24 @@ class Categories_Ingestor(Ingestor):
     # reion [init]
     target_table_name:str = Transaction_Categories_Table.__tablename__
     primary_key_col:str = inspect(Transaction_Categories_Table).primary_key[0].name
+    connector:MySQLConnector = MySQLConnector()
     # endregion
 
     # region [public]
-    def ingest_data(self,file_path:Union[Path,None],verbose:bool=True)-> None:
+    def ingest_data(self,file_path:Path,verbose:bool=True)-> None:
 
         if file_path is not None:
             df = self.read_data(file_path=file_path)
-        else:
-            df = self.read_data_internally()
 
         df = self.clean_data(df=df)
-        self._create_tables()
+        self.connector.create_tables()
 
         contained_ids = self.request_processed_ids()
 
         if not contained_ids:
             self.store_data(df=df)
             if verbose:
-                print(" -> Init: ", Transaction_Categories_Table.__tablename__)
+                print(" -> Recreated: ", Transaction_Categories_Table.__tablename__)
         else:
             if verbose:
                 print(" -> ", Transaction_Categories_Table.__tablename__,
@@ -55,19 +51,6 @@ class Categories_Ingestor(Ingestor):
     # endregion
 
     # region [private]
-    def _create_db_url(self,user:str,pw:str,host:str,db:str) -> str:
-        return f"mysql+pymysql://{user}:{pw}@{host}/{db}"
-
-    def _create_sql_engine(self) -> Any:
-        load_dotenv(find_dotenv())
-        return create_engine(self._create_db_url(user=os.getenv("USER"),
-                                                pw=os.getenv("PASSWORD"),
-                                                host=os.getenv("HOST"),
-                                                db=os.getenv("DATABASE")))
-
-    def _create_tables(self) -> None:
-        Base.metadata.create_all(bind=self._create_sql_engine())
-        return
     # endregion
 
     # region [class]
@@ -97,7 +80,7 @@ class Categories_Ingestor(Ingestor):
 
         return df
 
-    def read_data_internally(self) -> pd.DataFrame:
+    def read_data_from_models(self) -> pd.DataFrame:
 
         rows = []
         for lvl1_key, lvl1_val in TRANSACTION_CATEGORIES.items():
@@ -115,7 +98,7 @@ class Categories_Ingestor(Ingestor):
         return df
 
     def request_processed_ids(self) -> List[str]:
-        with Session(self._create_sql_engine()) as session:
+        with Session(self.connector.create_sql_engine()) as session:
             stmt = select(Transaction_Categories_Table.category_id).distinct()
             df = pd.read_sql(stmt,session.bind)
             res = list(df[self.primary_key_col].tolist())
@@ -123,7 +106,7 @@ class Categories_Ingestor(Ingestor):
 
     def store_data(self, df: pd.DataFrame,
                    if_exists:str='replace') -> None:
-        df.to_sql(con=self._create_sql_engine(),
+        df.to_sql(con=self.connector.create_sql_engine(),
                   name=self.target_table_name,
                   if_exists=if_exists,
                   index=False)
